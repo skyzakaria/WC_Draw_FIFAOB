9d5bded92572f97eac3bcb733ef065c5e0287819
Zakaria-Ishak Sekkai <zakaria-ishak_sekkai@etu.u-bourgogne.fr>
2026-08-17 23:03:21 +0200

    Function "Verify if we can add team to the group" added

--- Models/Group.cs
+++ Models/Group.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WC_Draw_FIFAOB.Models;
 
 namespace WC_Draw_FIFAOB.Models
 {
-    internal class Group
+    public class Group
     {
+        public char name { get; set; } // A, B ... H
+
+        public List<Team> Teams { get; set; } = new();
+
+        public Group(char name)
+        {
+            this.name = name;
+        }
     }
 }
--- Services/DrawService.cs
+++ Services/DrawService.cs
@@ -1,10 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WC_Draw_FIFAOB.Models;
 
 namespace WC_Draw_FIFAOB.Services
 {
-    internal class DrawService
+    public class DrawService
     {
+        // Function to verify if we can add a nation to the group
+        public bool CanAddTeamToGroup(Team team, Group group)
+        {
+            // Max number of teams allowed in a group is 4
+            if (group.Teams.Count >= 4)
+            {
+                return false;
+            }
+
+            // No more than 2 UEFA teams are allowed per group
+            // for the other confederations, no more than 1 team per group
+            int countConf = group.Teams.Count(t => t.Confederation == team.Confederation);
+            
+            if (team.Confederation == Confederation.UEFA && countConf >= 2)
+            {
+                return false;
+            } 
+
+            if (team.Confederation != Confederation.UEFA && countConf >=1)
+            {
+                return false;
+            }
+
+            return true;           
+        }
+
+        // Function 
     }
 }
