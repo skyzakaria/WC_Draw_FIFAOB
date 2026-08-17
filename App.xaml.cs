69ff20a5cc1792cb55ba24f82a059de440e6870a
Zakaria-Ishak Sekkai <zakaria-ishak_sekkai@etu.u-bourgogne.fr>
2026-08-17 20:31:43 +0200

    WC Teams add

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using WC_Draw_FIFAOB.Data;
 
 namespace WC_Draw_FIFAOB
 {
@@ -9,6 +10,14 @@ namespace WC_Draw_FIFAOB
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Intialize Trigger on startup
+            using var context = new WorldCupDbContext();
+            DbInitializer.Initialize(context);
+        }
     }
 
 }
--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -1,10 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WC_Draw_FIFAOB.Models;
 
 namespace WC_Draw_FIFAOB.Data
 {
-    internal class DbInitializer
+    public static class DbInitializer
     {
+        public static void Initialize(WorldCupDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Teams.Any()) return; // if BD contains already the data
+
+            var teams = new List<Team>
+            {
+                // Pot 1
+                new() { Name = "Brazil", Confederation = Confederation.CONCACAF, PotNumber = 1 },
+                new() { Name = "Argentina", Confederation = Confederation.CONCACAF, PotNumber = 1 },
+                new() { Name = "France", Confederation = Confederation.UEFA, PotNumber = 1 },
+                new() { Name = "Spain", Confederation = Confederation.UEFA, PotNumber = 1 },
+                new() { Name = "Portugal", Confederation = Confederation.UEFA, PotNumber = 1 },
+                new() { Name = "Germany", Confederation = Confederation.UEFA, PotNumber = 1 },
+                new() { Name = "England", Confederation = Confederation.UEFA, PotNumber = 1 },
+                new() { Name = "Colombia", Confederation = Confederation.CONCACAF, PotNumber = 1 },
+
+                // Pot 2
+                new() { Name = "Netherlands", Confederation = Confederation.UEFA, PotNumber = 2 },
+                new() { Name = "Morroco", Confederation = Confederation.CAF, PotNumber = 2 },
+                new() { Name = "Norway", Confederation = Confederation.UEFA, PotNumber = 2 },
+                new() { Name = "Mexico", Confederation = Confederation.CONCACAF, PotNumber = 2 },
+                new() { Name = "Japan", Confederation = Confederation.AFC, PotNumber = 2 },
+                new() { Name = "Croatia", Confederation = Confederation.UEFA, PotNumber = 2 },
+                new() { Name = "Belguim", Confederation = Confederation.UEFA, PotNumber = 2 },
+                new() { Name = "Uruguay", Confederation = Confederation.CONCACAF, PotNumber = 2 },
+
+                // Pot 3
+                new() { Name = "Swizerland", Confederation = Confederation.UEFA, PotNumber = 3 },
+                new() { Name = "Algeria", Confederation = Confederation.CAF, PotNumber = 3 },
+                new() { Name = "Australia", Confederation = Confederation.AFC, PotNumber = 3 },
+                new() { Name = "Senegal", Confederation = Confederation.CAF, PotNumber = 3 },
+                new() { Name = "Nigera", Confederation = Confederation.CAF, PotNumber = 3 },
+                new() { Name = "Serbia", Confederation = Confederation.UEFA, PotNumber = 3 },
+                new() { Name = "South Corea", Confederation = Confederation.AFC, PotNumber = 3 },
+                new() { Name = "USA", Confederation = Confederation.CONCACAF, PotNumber = 3 },
+
+                // Pot 4
+                new() { Name = "Poland", Confederation = Confederation.UEFA, PotNumber = 4 },
+                new() { Name = "Ivory Cost", Confederation = Confederation.CAF, PotNumber = 4 },
+                new() { Name = "Iran", Confederation = Confederation.AFC, PotNumber = 4 },
+                new() { Name = "Ghana", Confederation = Confederation.CAF, PotNumber = 4 },
+                new() { Name = "Costa Rica", Confederation = Confederation.CONCACAF, PotNumber = 4 },
+                new() { Name = "Italy", Confederation = Confederation.UEFA, PotNumber = 4 },
+                new() { Name = "Jamaica", Confederation = Confederation.CONCACAF, PotNumber = 4 },
+                new() { Name = "Saudi Arabia", Confederation = Confederation.AFC, PotNumber = 2 },
+            };
+
+            context.Teams.AddRange(teams);
+            context.SaveChanges();
     }
 }
