bfdf62a47b2f5b1398614d0cd0e753fa432dd3a5
Zakaria-Ishak Sekkai <zakaria-ishak_sekkai@etu.u-bourgogne.fr>
2026-08-19 23:06:35 +0200

    Added DrawPot fuction

--- Services/DrawService.cs
+++ Services/DrawService.cs
@@ -19,20 +19,41 @@ namespace WC_Draw_FIFAOB.Services
             // No more than 2 UEFA teams are allowed per group
             // for the other confederations, no more than 1 team per group
             int countConf = group.Teams.Count(t => t.Confederation == team.Confederation);
-            
-            if (team.Confederation == Confederation.UEFA && countConf >= 2)
-            {
-                return false;
-            } 
 
-            if (team.Confederation != Confederation.UEFA && countConf >=1)
+            if (team.Confederation == Confederation.UEFA && countConf >= 2)
             {
                 return false;
             }
 
-            return true;           
+            if (team.Confederation != Confederation.UEFA && countConf >= 1)
+            {
+                return false;
+            }
+
+            return true;
         }
 
-        // Function 
+        // Function to draw a Pot
+        public bool DrawPot(List<Group> groups, List<Team> potTeams, int potNumber)
+        {
+            // browse every team from the pot 
+            foreach (var team in potTeams)
+            {
+                // we are looking for the first valid group (A -> H)
+                var validGroup = groups.FirstOrDefault(g =>
+                    g.Teams.Count < potNumber && CanAddTeamToGroup(team, g));
+
+                // If no team can be added in the group, we return false
+                if (validGroup == null)
+                {
+                    return false;
+                }
+
+                // else we add the team
+                validGroup.Teams.Add(team);
+            }
+
+            return true;
+        }
     }
 }
